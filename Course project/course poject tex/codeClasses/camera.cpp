f91c208346dce1d7a4837b8a0cd73f6276f4985b
L3b1n <binleo07@icloud.com>
2024-06-28 21:46:18 +0300

    Add code for classes of course project tex

---
+++ Course project/course poject tex/codeClasses/camera.cpp
@@ -0,0 +1,9 @@
+void GMOD::setCameraProps(const int& camId, 
+                          const int& camResX, 
+                          const int& camResY, 
+                          const int& camFps) {
+  this->camId = camId;
+  this->camResX = camResX;
+  this->camResY = camResY;
+  this->camFps = camFps;
+}
