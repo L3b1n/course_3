e9ef16fbee6a5155e7bd210a22f9a244a3ed1b78
L3b1n <binleo07@icloud.com>
2024-07-14 21:25:12 +0300

    Add code for filter data of course work tex

---
+++ Course work/course work tex/codeBackgroundFilter/Threads.cpp
@@ -0,0 +1,10 @@
+void BackgroundFilter::filterUpdateThreads(const int32_t numThreads_ = 1) {
+    const uint32_t newNumThreads = numThreads_;
+
+    if (tf->modelSelection.empty() || tf->numThreads != newNumThreads) {
+        // Re-initialize model if it's not already the selected 
+        // one or switching inference device
+        tf->numThreads = newNumThreads;
+        initialized = false;
+    }
+}
